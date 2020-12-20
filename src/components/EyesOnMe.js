46e8bdc837caa693aaefcdd9a7460d6132fcf842
“saschakala” <mxsaschakala@gmail.com>
2020-12-20 15:42:21 -0500

    commit tree 18a0bc40421619914c5e3f00432ac78f5f6a85b0

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+    printGood = () => console.log('Good!')
+
+    printEyes = () => console.log('Hey! Eyes on me!')
+    
+    render() {
+        return(
+            <div>
+                <button onFocus={this.printGood} onBlur={this.printEyes}></button>
+            </div>
+        )}
+
+
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+    logPassword = () => {
+        console.log('Entering password...')
+    }
+    
+    render() {
+        return(
+            <div>
+                <input type="password" onKeyUp={this.logPassword}/>
+            </div>
+        )}
+
+
+    
+}
